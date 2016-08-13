e934218a6bdcfe3171dd2700c4e439e2031f2547
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2016-08-13 16:54:35 +0100

    directory intialised and database setup

---
+++ db/olympics.sql
@@ -0,0 +1,21 @@
+DROP TABLE nations;
+DROP TABLE athletes;
+DROP TABLE events;
+
+CREATE TABLE nations(
+id SERIAL8 PRIMARY KEY,
+name VARCHAR(255)
+);
+
+
+CREATE TABLE events(
+id SERIAL8 PRIMARY KEY,
+name VARCHAR(255)
+)
+
+CREATE TABLE athletes(
+id SERIAL8 PRIMARY KEY,
+name VARCHAR(255),
+nation_id int8 references nations(id) ON DELETE CASCADE,
+event_id int8 references events(id) ON DELETE CASCADE
+);
