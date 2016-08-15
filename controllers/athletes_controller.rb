fdd411b59d1c489d95f135a8e2dba392cfac71ef
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2016-08-15 09:43:54 +0100

    added the new restfull route to the athlete class and setup the nation and event controllers

--- controllers/athletes_controller.rb
+++ controllers/athletes_controller.rb
@@ -1,1 +1,9 @@
 require_relative '../models/athlete.rb'
+
+#NEW
+
+get('/athletes/new') do
+  @nations = Nation.all()
+  @events = Event.all
+  erb(:'athlete/new')
+end
--- main_controller.rb
+++ main_controller.rb
@@ -1,3 +1,5 @@
 require 'sinatra' 
 require 'sinatra/contrib/all' 
 require_relative './controllers/athletes_controller.rb'
+require_relative './controllers/nation_controller.rb'
+require_relative './controllers/event_controller.rb'
